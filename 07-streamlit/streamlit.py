364609491334a49d756350d88e2f0ae8054a5282
CarryARG <lucaas_carranza@hotmail.com>
2024-09-26 00:37:16 -0300

    prueba

--- 07-streamlit/streamlit.py
+++ 07-streamlit/streamlit.py
@@ -108,7 +108,6 @@ import streamlit as st
 import streamlit.components.v1 as components
 from PIL import Image
 from sklearn.neighbors import NearestNeighbors
-from st_on_hover_tabs import on_hover_tabs
 from streamlit_folium import folium_static, st_folium
 
 # Organizar las importaciones
