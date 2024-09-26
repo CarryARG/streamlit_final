b11055469e0ab84fa59a756fef3545e2693caa1a
CarryARG <lucaas_carranza@hotmail.com>
2024-09-26 02:54:16 -0300

    prueba

--- 07-streamlit/app.py
+++ 07-streamlit/app.py
@@ -16,6 +16,15 @@ st.title("Análisis de Sentimientos de Reviews de Walgreens")
 # Menú de navegación
 page = st.sidebar.selectbox("Selecciona una página:", ["Home", "Dashboard", "Modelos"])
 
+# Almacenamos el estado de la página seleccionada
+st.session_state.page = page
+
+# Al cambiar de página, forzamos la recarga
+if 'page' in st.session_state:
+    if st.session_state.page != page:
+        st.experimental_rerun()
+
+# Cargamos la página correspondiente
 if page == "Home":
     import home
 elif page == "Dashboard":
