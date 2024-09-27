bd1c7959fe780573ec19569bdb4148166842be95
Lucas Daniel Carranza <110673534+CarryARG@users.noreply.github.com>
2024-09-27 19:25:48 -0300

    Update modelos.py

--- 07-streamlit/modelos.py
+++ 07-streamlit/modelos.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import home  # Asegúrate de que home.py esté correctamente definido
+import dashboard  # Importa el archivo dashboard.py
+import modelos_ml  # Importa el archivo modelos_ml.py
+import base64
+from PIL import Image
 
 def modelos_ml_page():
     st.title("Modelos de Machine Learning")
