aea82f35fb0461add1634c2c5cc5af8a236c9ecd
Lucas Daniel Carranza <110673534+CarryARG@users.noreply.github.com>
2024-09-27 19:10:15 -0300

    Update dashboard.py

--- 07-streamlit/dashboard.py
+++ 07-streamlit/dashboard.py
@@ -1,13 +1,5 @@
 import streamlit as st
 
 def dashboard_page():
-    st.title("Dashboards")
-    
-    # Aquí puedes agregar el contenido exclusivo para la página de modelos
-    st.markdown("""
-    ## Bienvenido a la sección de Dashboards!
-    En esta sección podrás cargar, entrenar y evaluar modelos de Machine Learning.
-    """)
-    
-    # Ejemplo de espacio para futuros modelos
-    st.write("Aquí se mostrarán los modelos disponibles.")
+    st.title("Dashboard")
+    st.write("Aquí se mostrará el dashboard con gráficas y KPIs.")
