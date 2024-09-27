e9e18f2deab32aa3f17663b50b9a74554cc1254a
Lucas Daniel Carranza <110673534+CarryARG@users.noreply.github.com>
2024-09-27 19:10:24 -0300

    Update modelos.py

--- 07-streamlit/modelos.py
+++ 07-streamlit/modelos.py
@@ -1,13 +1,5 @@
 import streamlit as st
 
-def modelos_page():
+def modelos_ml_page():
     st.title("Modelos de Machine Learning")
-    
-    # Aquí puedes agregar el contenido exclusivo para la página de modelos
-    st.markdown("""
-    ## Bienvenido a la sección de Modelos de Machine Learning
-    En esta sección podrás cargar, entrenar y evaluar modelos de Machine Learning.
-    """)
-    
-    # Ejemplo de espacio para futuros modelos
-    st.write("Aquí se mostrarán los modelos disponibles.")
+    st.write("Aquí se mostrarán los modelos de Machine Learning.")
