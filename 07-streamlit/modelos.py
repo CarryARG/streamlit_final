2eea71d163655bcf746106fb85b6d3a51d18cc1c
Lucas Daniel Carranza <110673534+CarryARG@users.noreply.github.com>
2024-09-27 19:31:51 -0300

    Update modelos.py

--- 07-streamlit/modelos.py
+++ 07-streamlit/modelos.py
@@ -5,6 +5,6 @@ import modelos  # Importa el archivo modelos_ml.py
 import base64
 from PIL import Image
 
-def modelos_ml_page():
+def modelos_page():
     st.title("Modelos de Machine Learning")
     st.write("Aquí se mostrarán los modelos de Machine Learning.")
