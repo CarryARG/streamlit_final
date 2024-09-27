232fab0dbb588f1bbecc6e91da04de2268d6e5d3
Lucas Daniel Carranza <110673534+CarryARG@users.noreply.github.com>
2024-09-27 19:28:48 -0300

    Update app.py

--- 07-streamlit/app.py
+++ 07-streamlit/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import home  # Asegúrate de que home.py esté correctamente definido
 import dashboard  # Importa el archivo dashboard.py
-import modelos_ml  # Importa el archivo modelos_ml.py
+import modelos  # Importa el archivo modelos_ml.py
 import base64
 from PIL import Image
 
