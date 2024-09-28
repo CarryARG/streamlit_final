c68f49ad12fc16d9a1aef5d36a44bf0be1104de0
Lucas Daniel Carranza <110673534+CarryARG@users.noreply.github.com>
2024-09-28 12:15:25 -0300

    Update app.py

--- 07-streamlit/app.py
+++ 07-streamlit/app.py
@@ -41,9 +41,13 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
+
+
 # Obtener la página actual desde los parámetros de consulta (URL)
 query_params = st.query_params  # Usamos st.query_params
 page = query_params.get("page", ["home"])[0]  # Esto recupera el valor de 'page' en la URL
+
+
 
 # CSS para personalizar el navbar y eliminar los espacios sobrantes
 st.markdown(""" 
@@ -87,15 +91,6 @@ st.markdown("""
 
     </style>
 """, unsafe_allow_html=True)
-
-
-# Función para la navegación mediante URL de consulta (query parameter)
-def navigate_to(page):
-    st.experimental_set_query_params(page=page)
-
-# Obtener la página actual de los parámetros de consulta (URL)
-query_params = st.query_params
-page = query_params.get("page", ["home"])[0]
 
 
 # HTML para el Navbar utilizando Bootstrap con los botones personalizados
