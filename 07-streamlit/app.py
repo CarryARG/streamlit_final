9c5c740552e2a71e66d150052da4153d60665a3a
Lucas Daniel Carranza <110673534+CarryARG@users.noreply.github.com>
2024-09-28 12:17:10 -0300

    Update app.py

--- 07-streamlit/app.py
+++ 07-streamlit/app.py
@@ -4,104 +4,65 @@ import dashboard  # Importar la página 'dashboard.py'
 import modelos  # Importar la página 'modelos_ml.py'
 import base64
 
-# Configuración general de la página (debe ir antes de cualquier otro comando de Streamlit)
+# Configuración general de la página
 st.set_page_config(page_title="ARCOPE App", layout="wide", initial_sidebar_state="auto")
-# Limpiar la caché al cargar la página
-st.cache_data.clear()
-
-
-# Función para cargar imágenes en base64
-def get_image_b64(image_path):
-    try:
-        with open(image_path, "rb") as image_file:
-            return base64.b64encode(image_file.read()).decode()
-    except FileNotFoundError:
-        return None
+st.cache_data.clear()  # Limpiar la caché al cargar la página
 
 # Incluir Bootstrap CSS y JavaScript
 st.markdown("""
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
-    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" 
-    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" 
-    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9P/ScQsAP7hUibX39j13EVY4pQ11VVn1+kpZ60" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" 
-    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" crossorigin="anonymous"></script>
 """, unsafe_allow_html=True)
 
-# Ocultar los elementos no deseados del menú superior y el espaciado adicional
+# Ocultar elementos no deseados
 st.markdown("""
     <style>
-        #MainMenu {visibility: hidden;} /* Ocultar el menú de arriba a la derecha */
-        footer {visibility: hidden;} /* Ocultar el pie de página */
-        header {visibility: hidden;} /* Ocultar el header */
-        .css-18e3th9 {padding: 0;} /* Eliminar padding sobrante */
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+        header {visibility: hidden;}
+        .css-18e3th9 {padding: 0;}
     </style>
 """, unsafe_allow_html=True)
 
-
-
-# Obtener la página actual desde los parámetros de consulta (URL)
+# Obtener la página actual desde los parámetros de consulta
 query_params = st.query_params  # Usamos st.query_params
 page = query_params.get("page", ["home"])[0]  # Esto recupera el valor de 'page' en la URL
 
-
-
-# CSS para personalizar el navbar y eliminar los espacios sobrantes
-st.markdown(""" 
+# CSS para personalizar el navbar
+st.markdown("""
     <style>
-        #MainMenu, header, footer {
-            visibility: hidden;
+        .navbar-custom {
+            background-color: #000000;
+            padding: 20px;
+            position: fixed;
+            width: 100%;
+            top: 0;
+            left: 0;
+            z-index: 1000;
         }
-
         .nav-item {
             background-color: #F2A649;
             color: #FFF;
             padding: 10px 20px;
             border-radius: 5px;
+            margin-right: 10px; /* Espaciado entre botones */
         }
-
         .nav-item:hover {
-            background-color: #F25E3D; /* Color más oscuro para el hover */
-            box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2); /* Sombra suave */
+            background-color: #F25E3D; /* Color para hover */
         }
-
-        .navbar-custom {
-            background-color: #000000;
-            padding: 20px;
-            display: flex;
-            justify-content: space-around;
-            width: 100%; /* Ocupa todo el ancho de la pantalla */
-            margin: 0; /* Eliminar margen superior */
-            position: fixed; /* Fijar el navbar en la parte superior */
-            top: 0;
-            left: 0;
-            z-index: 1000; /* Asegurar que quede encima de otros elementos */
-        }
-
-        .css-18e3th9 {
-            padding: 0;
-        }
-
-        .main-content {
-            margin-top: 80px;  /* Ajusta según la altura del navbar */
-        }
-
     </style>
 """, unsafe_allow_html=True)
 
-
-# HTML para el Navbar utilizando Bootstrap con los botones personalizados
+# HTML para el Navbar utilizando Bootstrap
 st.markdown(f"""
     <nav class="navbar-custom">
-        <a href="#" class="nav-item" onclick="window.location.href='/?page=home'">Home</a>
-        <a href="#" class="nav-item" onclick="window.location.href='/?page=dashboard'">Dashboard</a>
-        <a href="#" class="nav-item" onclick="window.location.href='/?page=modelos'">Modelos</a>
+        <a class="nav-item" onclick="window.location.href='/?page=home'">Home</a>
+        <a class="nav-item" onclick="window.location.href='/?page=dashboard'">Dashboard</a>
+        <a class="nav-item" onclick="window.location.href='/?page=modelos'">Modelos</a>
     </nav>
 """, unsafe_allow_html=True)
-
 
 # Contenido basado en la página seleccionada
 if page == "home":
